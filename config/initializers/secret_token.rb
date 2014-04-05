ecbc89255f109d0773eea6bab3d6a41b253eae02
Brandonmchu <brandon.mk.chu@gmail.com>
2014-04-05 11:06:21 -0400

    initial commit of todo application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odot::Application.config.secret_key_base = '9c12a5e2cddd479232c24d0866f69ff134e9cc51d3c89d5812c98ac330ee279e2bc22fb77f2074141ec89cd685dbd688b9033685ac6e6d47181d196c3e986652'
